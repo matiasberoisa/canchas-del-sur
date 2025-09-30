37314d8c9f409060dbf5d4423ea7c8b673a21149
Camila <camila.castillo.manrique@gmail.com>
2025-09-29 23:13:13 -0300

    js para canchas catalogo

---
+++ canchas-del-sur/js/canchasCatalogo.js
@@ -0,0 +1,31 @@
+function cargarCanchasDisponibles() {
+    console.log("Cargando canchas disponibles...");
+  fetch("../utils/data/canchas.json")
+    .then((response) => response.json())
+    .then((data) => {
+      mostrarCanchas(data);
+    })
+    .catch((error) => {
+      console.error("Error al cargar las canchas:", error);
+    });
+}
+function mostrarCanchas(canchas) {
+  const canchasContainer = document.getElementById("resultadosCanchas");
+  canchasContainer.innerHTML = "";
+  canchas.forEach(cancha => {
+    canchasContainer.innerHTML += `
+      <section id="canchaData">
+        <img src="${cancha.imagen}" alt="">
+        <p id="canchaNombre">${cancha.tipo}</p>
+        <p id="canchaUbicacion">
+          <i class="fa-solid fa-location-dot" style="color: gray ; margin-right: 0.3rem;"></i>
+          ${cancha.ubicacion}
+        </p>
+        <section id="horarios">
+          ${cancha.horarios.map(h => `<p>${h}</p>`).join("")}
+        </section>
+        <button onclick="location.href='canchaDetalle.html?id=${cancha.id}'">Reservar</button>
+      </section>
+    `;
+  });
+}
